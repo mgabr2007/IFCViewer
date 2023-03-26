16a706c2a83fe2d9938d8300531e05a91617a3cb
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-26 12:53:39 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,82 +1,40 @@
 import ifcopenshell
-import pyvista as pv
+import pandas as pd
 import streamlit as st
-import os
-os.environ["VTK_CONFIG_FILE"] = "/path/to/vtk-config.cmake"
-
-def visualize_geometry(file_path, lod_level):
-    # Open the IFC file
-    ifc_file = ifcopenshell.open(file_path)
-
-    # Extract the geometry of all the building elements in the file
-    building_elements = ifc_file.by_type("IfcBuildingElement")
-    meshes = []
-    for element in building_elements:
-        geometry = ifcopenshell.geom.create_shape(settings=ifcopenshell.geom.settings(lod=lod_level), ifc_entity=element)
-        if geometry:
-            meshes.append(pv.PolyData(geometry.geometry))
-
-    # Create a visualization and add the element meshes to it
-    p = pv.Plotter()
-
-    # Add the meshes to a LOD actor
-    actor = pv.LODActor()
-    for i, mesh in enumerate(meshes):
-        actor.add_mesh(mesh, lod_resolution=i)
-
-    p.add_actor(actor)
-    return p
-
-# Set up the Streamlit app
-st.set_page_config(
-    layout= "wide",
-    page_title="IFC Stream",
-    page_icon="✍️",
-)
-st.title("Streamlit IFC")
-
-# Add File uploader to Side Bar Navigation
-st.sidebar.header('Model Loader')
-uploaded_file = st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file")
-
-# Allow user to choose LOD level
-if uploaded_file:
-    lod_level = st.sidebar.slider("LOD Level", min_value=0, max_value=10, value=1, step=1)
-    st.sidebar.success(f"LOD level set to {lod_level}")
-
-# Load the IFC file and display the visualization
-if uploaded_file:
-    callback_upload()
-    file_path = session["file_name"]
-    p = visualize_geometry(file_path, lod_level)
-    st.write(p.show())
 
 def callback_upload():
-    session["file_name"] = uploaded_file.name
-    session["array_buffer"] = uploaded_file.getvalue()
+    session["file_name"] = session["uploaded_file"].name
+    session["array_buffer"] = session["uploaded_file"].getvalue()
     session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
     session["is_file_loaded"] = True
     
-    ### Empty Previous Model Data from Session State
-    session["isHealthDataLoaded"] = False
-    session["HealthData"] = {}
-    session["Graphs"] = {}
-    session["SequenceData"] = {}
-    session["CostScheduleData"] = {}
+    # Extract data from IFC file
+    walls = session["ifc_file"].by_type("IfcWall")
+    windows = session["ifc_file"].by_type("IfcWindow")
+    doors = session["ifc_file"].by_type("IfcDoor")
+    
+    # Store data in session state
+    session["data"] = {
+        "walls": pd.DataFrame(walls),
+        "windows": pd.DataFrame(windows),
+        "doors": pd.DataFrame(doors)
+    }
 
-    ### Empty Previous DataFrame from Session State
-    session["DataFrame"] = None
-    session["Classes"] = []
-    session["IsDataFrameLoaded"] = False
+    # Clear previous comparison results
+    session["comparison"] = None
 
-def get_project_name():
-    return session["ifc_file"].by_type("IfcProject")[0].Name
-
-def change_project_name():
-    if session["project_name_input"]:
-        session["ifc_file"].by_type("IfcProject")[0].Name = session["project_name_input"]
-        st.success('Project name successfully updated!')
-        session["popup_shown"] = True
+def compare_components():
+    # Get user input for components to compare
+    component_1 = st.selectbox("Select first component:", ["walls", "windows", "doors"])
+    component_2 = st.selectbox("Select second component:", ["walls", "windows", "doors"])
+    
+    # Compare selected components
+    data_1 = session["data"][component_1]
+    data_2 = session["data"][component_2]
+    comparison = pd.concat([data_1.describe(), data_2.describe()], keys=[component_1, component_2], axis=1)
+    
+    # Store comparison results in session state
+    session["comparison"] = comparison
 
 def main():      
     st.set_page_config(
@@ -91,28 +49,47 @@ def main():
     """
     )
 
+    ## Add File uploader to Side Bar Navigation
+    st.sidebar.header('Model Loader')
+    st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
+
     ## Add File Name and Success Message
     if "is_file_loaded" in session and session["is_file_loaded"]:
         st.sidebar.success(f'Project successfuly loaded')
         st.sidebar.write("🔃 You can reload a new file  ")
         
-        col1.subheader(f'Start Exploring "{get_project_name()}"')
+        # Add comparison section
+        if "data" in session:
+            st.sidebar.write("----")
+            st.sidebar.header("Compare Components")
+            compare_components()
+            if session["comparison"] is not None:
+                st.write(session["comparison"])
+            
+        # Add section to display data from IFC file
+        st.sidebar.write("----")
+        st.sidebar.header("IFC Data")
+        data_to_display = st.sidebar.selectbox("Select component to display:", ["walls", "windows", "doors"])
+        st.write(session["data"][data_to_display])
+        
+        # Add section to change project name
+        col1, col2 = st.columns([2,1])
+        col1.subheader(f'Start Exploring "{session["ifc_file"].by_type("IfcProject")[0].Name}"')
         col2.text_input("✏️ Change Project Name", key="project_name_input")
-        col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name)
-
-    if session["popup_shown"]:
-        st.success('Project name successfully updated!')
-        session["popup_shown"] = False
-
-st.sidebar.write("""
+        col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name())
+        st.sidebar.write("""
 --------------
+### Credits:
+#### Sigma Dimensions (TM)
+
+Follow us [on Youtube](https://www.youtube.com/channel/UC9bPwuJZUD6ooKqzwdq9M9Q?sub_confirmation=1)
+
+--------------
+License: MIT
+
 """)
 st.write("")
 st.sidebar.write("")
-
-if __name__ == "main":
-    session = st.session_state
-    session["popup_shown"] = False
-    main()
-
-
+if name == "main":
+session = st.session_state
+main()
