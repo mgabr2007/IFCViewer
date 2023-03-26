eb27a3359e5309d3d2a6c8abd4001a4a6ec1cfa3
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-26 13:49:37 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,6 +1,52 @@
 import ifcopenshell
-import pandas as pd
 import streamlit as st
+import pandas as pd
+
+def callback_upload():
+    session["file_name"] = session["uploaded_file"].name
+    session["array_buffer"] = session["uploaded_file"].getvalue()
+    session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
+    session["is_file_loaded"] = True
+    
+    ### Empty Previous Model Data from Session State
+    session["isHealthDataLoaded"] = False
+    session["HealthData"] = {}
+    session["Graphs"] = {}
+    session["SequenceData"] = {}
+    session["CostScheduleData"] = {}
+
+    ### Empty Previous DataFrame from Session State
+    session["DataFrame"] = None
+    session["Classes"] = []
+    session["IsDataFrameLoaded"] = False
+
+    # Extract available components from IFC file
+    available_components = ["Pick Component"]
+    for component in ["IfcWall", "IfcWindow", "IfcDoor"]:
+        available_components.extend([c.Name for c in session["ifc_file"].by_type(component)])
+
+    # Store available components in session state
+    session["available_components"] = available_components
+
+    # Extract data from IFC file
+    walls = session["ifc_file"].by_type("IfcWall")
+    windows = session["ifc_file"].by_type("IfcWindow")
+    doors = session["ifc_file"].by_type("IfcDoor")
+
+    # Store data in session state
+    session["data"] = {
+        "walls": pd.DataFrame(walls),
+        "windows": pd.DataFrame(windows),
+        "doors": pd.DataFrame(doors)
+    }
+
+def get_project_name():
+    return session.ifc_file.by_type("IfcProject")[0].Name
+
+def change_project_name():
+    if session.project_name_input:
+        session.ifc_file.by_type("IfcProject")[0].Name = session.project_name_input
+        st.balloons()
 
 def main():      
     st.set_page_config(
@@ -10,62 +56,50 @@ def main():
     )
     st.title("Streamlit IFC")
     st.markdown(
-        """ 
-        ### 📁 Upload your IFC file
-        """
+    """ 
+    ###  📁 Click on Browse File in the Side Bar to start
+    """
     )
 
-    uploaded_file = st.file_uploader("Choose a file", type=['ifc', 'IFC'])
+    ## Add File uploader to Side Bar Navigation
+    st.sidebar.header('Model Loader')
+    st.sidebar.file_uploader("Choose a file", type=['ifc', 'IFC'], key="uploaded_file", on_change=callback_upload)
 
-    if uploaded_file is not None:
-        # Load IFC file from uploaded file
-        ifc_file = ifcopenshell.open(uploaded_file)
-
-        # Extract available components from IFC file
-        available_components = ["Pick Component"]
-        for component in ["IfcWall", "IfcWindow", "IfcDoor"]:
-            available_components.extend([c.Name for c in ifc_file.by_type(component)])
-
-        # Store available components in session state
-        session["available_components"] = available_components
-
-        # Extract data from IFC file
-        walls = ifc_file.by_type("IfcWall")
-        windows = ifc_file.by_type("IfcWindow")
-        doors = ifc_file.by_type("IfcDoor")
-
-        # Store data in session state
-        session["data"] = {
-            "walls": pd.DataFrame(walls),
-            "windows": pd.DataFrame(windows),
-            "doors": pd.DataFrame(doors)
-        }
-
-        # Clear previous comparison results
-        session["comparison"] = None
+    ## Add File Name and Success Message
+    if "is_file_loaded" in session and session["is_file_loaded"]:
+        st.sidebar.success(f'Project successfuly loaded')
+        st.sidebar.write("🔃 You can reload a new file  ")
         
-    # Add component selection dropdown
-    if "available_components" in session:
-        data_to_display = st.selectbox("Select component to display:", session["available_components"])
-
-        # Check if data to display is present in session state
-        if data_to_display not in session["data"]:
-            st.warning("No data found for selected component.")
-            return
-
-        # Display selected component data
-        st.write(session["data"][data_to_display])
-
         col1, col2 = st.columns([2,1])
         col1.subheader(f'Start Exploring "{get_project_name()}"')
         col2.text_input("✏️ Change Project Name", key="project_name_input")
         col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name())
 
+        # Add component selection dropdown
+        if "available_components" in session:
+            data_to_display = st.selectbox("Select component to display:", session["available_components"])
+
+            # Check if data to display is present in session state
+            if data_to_display not in session["data"]:
+                st.warning("No data found for selected component.")
+            else:
+                # Display selected component data
+                st.write(session["data"][data_to_display])
+
     st.sidebar.write("""
     --------------
+    ### Credits:
+    #### Sigma Dimensions (TM)
+    
+    Follow us [on Youtube](https://www.youtube.com/channel/UC9bPwuJZUD6ooKqzwdq9M9Q?sub_confirmation=1)
+    
     --------------
+    License: MIT
     
     """)
     st.write("")
     st.sidebar.write("")
 
+if __name__ == "__main__":
+    session = st.session_state
+    main()
