492c5d7afc222ee870dd2b786343110e4da813bc
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-28 13:20:58 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,46 +1,62 @@
 import ifcopenshell
-import ifcopenshell.geom
 import py3Dmol
 import streamlit as st
 import pandas as pd
-import tempfile
 import os
+import subprocess
 
 def convert_ifc_to_obj(ifc_file_path, obj_file_path):
-    settings = ifcopenshell.geom.settings()
-    settings.set(settings.USE_PYTHON_OPENCASCADE, True)
-    ifc_file = ifcopenshell.open(ifc_file_path)
+    ifc_convert_executable = './IfcConvert'  # Path to the IfcConvert executable
+    if not os.path.exists(ifc_convert_executable):
+        raise FileNotFoundError(f'IfcConvert executable not found at {ifc_convert_executable}')
 
-    with open(obj_file_path, "w") as obj_file:
-        for product in ifc_file.by_type("IfcProduct"):
-            try:
-                shape = ifcopenshell.geom.create_shape(settings, product)
-                obj_data = shape.geometry.to_obj()
-                obj_file.write(obj_data)
-            except Exception as e:
-                print(f"Failed to process {product}: {e}")
+    command = f"{ifc_convert_executable} {ifc_file_path} {obj_file_path}"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Error during IFC to OBJ conversion: {result.stderr.decode('utf-8')}")
 
 def callback_upload():
+    session["file_name"] = session["uploaded_file"].name
+    session["array_buffer"] = session["uploaded_file"].getvalue()
+    session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
+    session["is_file_loaded"] = True
     if "uploaded_file" not in session or session["uploaded_file"] is None:
         return
 
+    session["file_name"] = session["uploaded_file"].name
+
     # Save the uploaded IFC file to disk
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".ifc") as ifc_file:
-        ifc_file.write(session["uploaded_file"].getvalue())
-        ifc_file_path = ifc_file.name
+    ifc_file_path = f"temp_{session['file_name']}"
+    with open(ifc_file_path, "wb") as ifc_file:
+        ifc_file.write(session["array_buffer"])
 
     # Convert the IFC file to an OBJ file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".obj") as obj_file:
-        obj_file_path = obj_file.name
-
+    obj_file_path = f"temp_{session['file_name']}.obj"
     convert_ifc_to_obj(ifc_file_path, obj_file_path)
 
     # Store the OBJ file path in the session state
     session["obj_file_path"] = obj_file_path
 
-    # Read the IFC file using ifcopenshell
-    session["ifc_file"] = ifcopenshell.open(ifc_file_path)
-    session["is_file_loaded"] = True
+    # Extract data from IFC file
+    walls = session["ifc_file"].by_type("IfcWall")
+    windows = session["ifc_file"].by_type("IfcWindow")
+    doors = session["ifc_file"].by_type("IfcDoor")
+
+    # Store data in session state
+    session["data"] = {
+        "walls": pd.DataFrame(walls),
+        "windows": pd.DataFrame(windows),
+        "doors": pd.DataFrame(doors),
+    }
+
+def get_project_name():
+    return session.ifc_file.by_type("IfcProject")[0].Name
+
+def change_project_name():
+    if session.project_name_input:
+        session.ifc_file.by_type("IfcProject")[0].Name = session.project_name_input
+        st.sidebar.success("Project name changed successfully.")
 
 def show_obj(obj_file_path):
     with open(obj_file_path, 'r') as obj_file:
@@ -57,6 +73,10 @@ def main():
     if "is_file_loaded" not in session:
         session["is_file_loaded"] = False
 
+    if "is_file_loaded" in session and session["is_file_loaded"]:
+        # Display the 3D model
+        show_obj(session["obj_file_path"])
+    
     st.set_page_config(
         layout= "wide",
         page_title="IFC Stream",
@@ -65,7 +85,7 @@ def main():
     st.title("Streamlit IFC")
     st.markdown(
     """ 
-    ###  📁 Click on Browse File in the Side Bar to start
+    ###   📁 Click on Browse File in the Side Bar to start
     """
     )
 
