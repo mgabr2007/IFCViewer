ae216e6f3c7fef2425e8e04b23de8bac4d758f19
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-29 12:51:44 +0200

    Update windows_info.py

--- pages/windows_info.py
+++ pages/windows_info.py
@@ -1,31 +1,30 @@
-# windows_info.py
+# pages/windows_info.py
 import streamlit as st
-import ifcopenshell
+import pandas as pd
 
 def get_windows_info(ifc_file):
     windows = ifc_file.by_type("IfcWindow")
     windows_info = []
 
     for window in windows:
-        window_data = {
+        info = {
             "GlobalId": window.GlobalId,
             "Name": window.Name,
             "ObjectType": window.ObjectType,
+            "OverallWidth": window.OverallWidth,
             "OverallHeight": window.OverallHeight,
-            "OverallWidth": window.OverallWidth
         }
-        windows_info.append(window_data)
+        windows_info.append(info)
 
     return windows_info
 
 def windows_info_page(ifc_file):
-    st.title("Windows Information")
-
     if ifc_file:
         windows_info = get_windows_info(ifc_file)
         if windows_info:
             st.write("Windows found in the IFC file:")
-            st.table(windows_info)
+            df_windows_info = pd.DataFrame(windows_info)
+            st.write(df_windows_info)
         else:
             st.warning("No windows found in the IFC file.")
     else:
