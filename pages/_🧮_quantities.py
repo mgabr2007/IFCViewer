61e3880d70518fde47579f7fb360bf29f43ed2f5
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-29 15:05:54 +0200

    Update _🧮_quantities.py

--- pages/_🧮_quantities.py
+++ pages/_🧮_quantities.py
@@ -2,6 +2,7 @@ import streamlit as st
 from tools import graph_maker
 from tools import ifchelper
 from tools import pandashelper
+import base64
 
 session = st.session_state
 
@@ -36,6 +37,21 @@ def download_excel():
     pandashelper.download_excel(session.file_name, session.DataFrame)
 
 
+def download_filtered_csv():
+    # Filter the DataFrame based on user input
+    filtered_df = pandashelper.filter_dataframe_per_class(session.DataFrame, session.class_selector)
+    filtered_df = pandashelper.get_quantities(filtered_df, session.qto_selector)
+
+    # Generate a downloadable link for the CSV file
+    csv_file = filtered_df.to_csv(index=False)
+    b64 = base64.b64encode(csv_file.encode()).decode()
+    button_label = f"Download {session.class_selector} - {session.qto_selector}.csv"
+    href = f'<a href="data:file/csv;base64,{b64}" download="{session.class_selector} - {session.qto_selector}.csv"><button>{button_label}</button></a>'
+
+    # Display the download button
+    st.markdown(href, unsafe_allow_html=True)
+
+
 def execute():
     st.set_page_config(
         page_title="Quantities",
@@ -50,7 +66,7 @@ def execute():
     if session.IsDataFrameLoaded:
         tab1, tab2 = st.tabs(["Dataframe Utilities", "Quantities Review"])
         with tab1:
-            ## DATAFRAME REVIEW            
+            ## DATAFRAME REVIEW
             st.header("DataFrame Review")
             st.write(session.DataFrame)
             # from st_aggrid import AgGrid
@@ -70,25 +86,25 @@ def execute():
                         quantities = pandashelper.get_quantities(session.filtered_frame, session.qto_selector)
                         st.selectbox("Select Quantity", quantities, key="quantity_selector")
                         st.radio('Split per', ['Level', 'Type'], key="split_options")
-                    else:
-                        st.warning("No Quantities to Look at !")
-            ## DRAW FRAME
-            with row2col2:
-                if "quantity_selector" in session and session.quantity_selector == "Count":
-                    total = pandashelper.get_total(session.filtered_frame)
-                    st.write(f"The total number of {session.class_selector} is {total}")
+                        st.button("Download Filtered CSV", key="download_filtered_csv", on_click=download_filtered_csv)
                 else:
-                    if session.qtos is not None:
-                        st.subheader(f"{session.class_selector} {session.quantity_selector}")
-                        graph = graph_maker.load_graph(
-                            session.filtered_frame,
-                            session.qto_selector,
-                            session.quantity_selector,
-                            session.split_options,
-                        )
-                        st.plotly_chart(graph)
-    else:
-        st.header("Step 1: Load a file from the Home Page")
-
+                    st.warning("No Quantities to Look at !")
+        ## DRAW FRAME
+        with row2col2:
+            if "quantity_selector" in session and session.quantity_selector == "Count":
+                total = pandashelper.get_total(session.filtered_frame)
+                st.write(f"The total number of {session.class_selector} is {total}")
+            else:
+                if session.qtos is not None:
+                    st.subheader(f"{session.class_selector} {session.quantity_selector}")
+                    graph = graph_maker.load_graph(
+                        session.filtered_frame,
+                        session.qto_selector,
+                        session.quantity_selector,
+                        session.split_options,
+                    )
+                    st.plotly_chart(graph)
+else:
+    st.header("Step 1: Load a file from the Home Page")
 
 execute()
