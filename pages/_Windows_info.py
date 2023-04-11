fb853d74ac8d21d1cd51162a2d75246a0fdd85de
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-04-12 00:51:07 +0200

    Update _Windows_info.py

--- pages/_Windows_info.py
+++ pages/_Windows_info.py
@@ -11,8 +11,19 @@ def get_windows_info(ifc_file):
             "GlobalId": window.GlobalId,
             "Name": window.Name,
             "ObjectType": window.ObjectType,
+            "ConstructionType": window.ConstructionType.Name if window.ConstructionType else None,
+            "FrameDepth": window.FrameDepth,
+            "GlazingType": window.GlazingType.Name if window.GlazingType else None,
+            "OperationType": window.OperationType.Name if window.OperationType else None,
+            "SoundReductionIndex": window.SoundReductionIndex,
             "OverallWidth": window.OverallWidth,
             "OverallHeight": window.OverallHeight,
+            "Location": window.ObjectPlacement.RelativePlacement.Location.Coordinates if window.ObjectPlacement.RelativePlacement.Location else None,
+            "Elevation": window.ObjectPlacement.RelativePlacement.RefDirection.DirectionRatios if window.ObjectPlacement.RelativePlacement.RefDirection else None,
+            "Orientation": window.ObjectPlacement.RelativePlacement.PlacementRelTo.RelativePlacement.RefDirection.DirectionRatios if window.ObjectPlacement.RelativePlacement.PlacementRelTo and window.ObjectPlacement.RelativePlacement.PlacementRelTo.RelativePlacement.RefDirection else None,
+            "Zone": window.ContainedInStructure.Name if window.ContainedInStructure else None,
+            "UValue": window.HasPropertySets[0].HasProperties[0].NominalValue.wrappedValue if window.HasPropertySets and window.HasPropertySets[0].HasProperties and window.HasPropertySets[0].HasProperties[0].Name == "U-Value" else None,
+            "SHGC": window.HasPropertySets[0].HasProperties[0].NominalValue.wrappedValue if window.HasPropertySets and window.HasPropertySets[0].HasProperties and window.HasPropertySets[0].HasProperties[0].Name == "SHGC" else None
         }
         windows_info.append(info)
 
