8978ddcccf30d0ec0ddd8648bc819519967793d7
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-04-15 16:34:54 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,31 +1,11 @@
 import ifcopenshell
 import streamlit as st
+import os
+import glob
+import importlib
 
-def callback_upload():
-    st.session_state["file_name"] = st.session_state["uploaded_file"].name
-    st.session_state["array_buffer"] = st.session_state["uploaded_file"].getvalue()
-    st.session_state["ifc_file"] = ifcopenshell.file.from_string(st.session_state["array_buffer"].decode("utf-8"))
-    st.session_state["is_file_loaded"] = True
-    
-    ### Empty Previous Model Data from Session State
-    st.session_state["isHealthDataLoaded"] = False
-    st.session_state["HealthData"] = {}
-    st.session_state["Graphs"] = {}
-    st.session_state["SequenceData"] = {}
-    st.session_state["CostScheduleData"] = {}
-
-    ### Empty Previous DataFrame from Session State
-    st.session_state["DataFrame"] = None
-    st.session_state["Classes"] = []
-    st.session_state["IsDataFrameLoaded"] = False
-
-def get_project_name():
-    return st.session_state.ifc_file.by_type("IfcProject")[0].Name
-
-def change_project_name():
-    if st.session_state.project_name_input:
-        st.session_state.ifc_file.by_type("IfcProject")[0].Name = st.session_state.project_name_input
-        st.balloons()
+def render_page(page_module):
+    page_module.show()
 
 def main():      
     st.set_page_config(
@@ -40,11 +20,9 @@ def main():
     """
     )
 
-    ## Add File uploader to Side Bar Navigation
     st.sidebar.header('Model Loader')
     st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
 
-    ## Add File Name and Success Message
     if "is_file_loaded" in st.session_state and st.session_state["is_file_loaded"]:
         st.sidebar.success(f'Project successfuly loaded')
         st.sidebar.write("🔃 You can reload a new file  ")
@@ -63,6 +41,26 @@ def main():
     """)
     st.write("")
     st.sidebar.write("")
+
+    # Get all .py files from the "pages" folder
+    pages_folder = 'pages'
+    py_files = glob.glob(os.path.join(pages_folder, "*.py"))
+
+    # Get the module names and display names for the pages
+    module_names = [os.path.splitext(os.path.basename(file))[0] for file in py_files]
+    display_names = [name.replace('_', ' ').title() for name in module_names]
+
+    # Create a dictionary to map display names to their corresponding modules
+    page_modules = {}
+    for module_name, display_name in zip(module_names, display_names):
+        module = importlib.import_module(f'pages.{module_name}')
+        page_modules[display_name] = module
+
+    # Add the selectbox to the sidebar to choose between the pages
+    selected_page = st.sidebar.selectbox('Choose a Page', display_names)
+
+    # Render the selected page
+    render_page(page_modules[selected_page])
 
 if __name__ == "__main__":
     main()
