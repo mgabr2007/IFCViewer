24dd458949f905bbb078b26003fc6156a48c6179
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-04-15 14:23:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
-
-# Get the current version of the Streamlit executable
-STREAMLIT_VERSION=$(streamlit --version)
-
-# Add Streamlit to the allow list
-echo "web: $STREAMLIT_VERSION" > ~/.streamlit/config.toml
-
-# Create directories required for Streamlit
-mkdir -p ~/.streamlit
-touch ~/.streamlit/credentials.toml
-touch ~/.streamlit/config.toml
-
-# Set up the credentials file
-echo "[general]" > ~/.streamlit/credentials.toml
-echo "email = \"m.gabr@aucegypt.edu\"" >> ~/.streamlit/credentials.toml
-
-# Set up the config file
-echo "[server]" > ~/.streamlit/config.toml
-echo "headless = true" >> ~/.streamlit/config.toml
-echo "enableCORS=false" >> ~/.streamlit/config.toml
-echo "port = $PORT" >> ~/.streamlit/config.toml
+mkdir -p /app/.streamlit
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > /app/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > /app/.streamlit/config.toml
