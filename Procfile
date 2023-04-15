04f6345c5a501d92eaa3308ded5ab97d0975f35a
Mostafa Gabr <m.gabr@aucegypt.edu>
2023-04-15 13:18:40 +0200

    Update Streamlit version and config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run --server.port $PORT --server.enableCORS false --server.enableXsrfProtection false Homepage.py
+web: sh setup.sh && streamlit run --server.enableCORS false --server.port $PORT Homepage.py
--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
-mkdir -p ~/.streamlit/
+#!/bin/bash
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+# Get the current version of the Streamlit executable
+STREAMLIT_VERSION=$(streamlit --version)
+
+# Add Streamlit to the allow list
+echo "web: $STREAMLIT_VERSION" > ~/.streamlit/config.toml
+
+# Create directories required for Streamlit
+mkdir -p ~/.streamlit
+touch ~/.streamlit/credentials.toml
+touch ~/.streamlit/config.toml
+
+# Set up the credentials file
+echo "[general]" > ~/.streamlit/credentials.toml
+echo "email = \"m.gabr@aucegypt.edu\"" >> ~/.streamlit/credentials.toml
+
+# Set up the config file
+echo "[server]" > ~/.streamlit/config.toml
+echo "headless = true" >> ~/.streamlit/config.toml
+echo "enableCORS=false" >> ~/.streamlit/config.toml
+echo "port = $PORT" >> ~/.streamlit/config.toml
