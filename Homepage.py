eb3279db88268fb63924d9d35f580dcada956f49
Mostafa Gabr <m.gabr@aucegypt.edu>
2023-04-15 13:00:20 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,29 +2,29 @@ import ifcopenshell
 import streamlit as st
 
 def callback_upload():
-    session["file_name"] = session["uploaded_file"].name
-    session["array_buffer"] = session["uploaded_file"].getvalue()
-    session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
-    session["is_file_loaded"] = True
+    st.session_state["file_name"] = st.session_state["uploaded_file"].name
+    st.session_state["array_buffer"] = st.session_state["uploaded_file"].getvalue()
+    st.session_state["ifc_file"] = ifcopenshell.file.from_string(st.session_state["array_buffer"].decode("utf-8"))
+    st.session_state["is_file_loaded"] = True
     
     ### Empty Previous Model Data from Session State
-    session["isHealthDataLoaded"] = False
-    session["HealthData"] = {}
-    session["Graphs"] = {}
-    session["SequenceData"] = {}
-    session["CostScheduleData"] = {}
+    st.session_state["isHealthDataLoaded"] = False
+    st.session_state["HealthData"] = {}
+    st.session_state["Graphs"] = {}
+    st.session_state["SequenceData"] = {}
+    st.session_state["CostScheduleData"] = {}
 
     ### Empty Previous DataFrame from Session State
-    session["DataFrame"] = None
-    session["Classes"] = []
-    session["IsDataFrameLoaded"] = False
+    st.session_state["DataFrame"] = None
+    st.session_state["Classes"] = []
+    st.session_state["IsDataFrameLoaded"] = False
 
 def get_project_name():
-    return session.ifc_file.by_type("IfcProject")[0].Name
+    return st.session_state.ifc_file.by_type("IfcProject")[0].Name
 
 def change_project_name():
-    if session.project_name_input:
-        session.ifc_file.by_type("IfcProject")[0].Name = session.project_name_input
+    if st.session_state.project_name_input:
+        st.session_state.ifc_file.by_type("IfcProject")[0].Name = st.session_state.project_name_input
         st.balloons()
 
 def main():      
@@ -45,7 +45,7 @@ def main():
     st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
 
     ## Add File Name and Success Message
-    if "is_file_loaded" in session and session["is_file_loaded"]:
+    if "is_file_loaded" in st.session_state and st.session_state["is_file_loaded"]:
         st.sidebar.success(f'Project successfuly loaded')
         st.sidebar.write("🔃 You can reload a new file  ")
         
@@ -65,5 +65,4 @@ def main():
     st.sidebar.write("")
 
 if __name__ == "__main__":
-    session = st.session_state
     main()
