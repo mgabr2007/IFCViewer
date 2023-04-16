89be9291afa3cfbca96f31744f5b6c1b1c5e9cc9
Mostafa Gabr <m.gabr@aucegypt.edu>
2023-04-16 14:37:51 +0200

    Modify 7

--- Homepage.py
+++ Homepage.py
@@ -3,34 +3,32 @@ import streamlit as st
 import os
 import glob
 import importlib
+
 def callback_upload():
-    session["file_name"] = session["uploaded_file"].name
-    session["array_buffer"] = session["uploaded_file"].getvalue()
-    session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
-    session["is_file_loaded"] = True
+    session.file_name = session.uploaded_file.name
+    session.array_buffer = session.uploaded_file.getvalue()
+    session.ifc_file = ifcopenshell.file.from_string(session.array_buffer.decode("utf-8"))
+    session.is_file_loaded = True
 
-    ### Empty Previous Model Data from Session State
-    session["isHealthDataLoaded"] = False
-    session["HealthData"] = {}
-    session["Graphs"] = {}
-    session["SequenceData"] = {}
-    session["CostScheduleData"] = {}
+    # Empty Previous Model Data from Session State
+    session.isHealthDataLoaded = False
+    session.HealthData = {}
+    session.Graphs = {}
+    session.SequenceData = {}
+    session.CostScheduleData = {}
 
-    ### Empty Previous DataFrame from Session State
-    session["DataFrame"] = None
-    session["Classes"] = []
-    session["IsDataFrameLoaded"] = False
-
+    # Empty Previous DataFrame from Session State
+    session.DataFrame = None
+    session.Classes = []
+    session.IsDataFrameLoaded = False
 
 def get_project_name():
     return session.ifc_file.by_type("IfcProject")[0].Name
-
 
 def change_project_name():
     if session.project_name_input:
         session.ifc_file.by_type("IfcProject")[0].Name = session.project_name_input
         st.balloons()
-
 
 def main():
     st.set_page_config(
@@ -45,21 +43,20 @@ def main():
         """
     )
 
-    ## Add File uploader to Side Bar Navigation
+    # Add File uploader to Side Bar Navigation
     st.sidebar.header('Model Loader')
     st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
 
-    ## Add File Name and Success Message
-    if "is_file_loaded" in session and session["is_file_loaded"]:
-        st.sidebar.success(f'Project successfuly loaded')
+    # Add File Name and Success Message
+    if "is_file_loaded" in session and session.is_file_loaded:
+        st.sidebar.success(f'Project successfully loaded')
         st.sidebar.write("🔃 You can reload a new file  ")
 
         col1, col2 = st.columns([2, 1])
         col1.subheader(f'Start Exploring "{get_project_name()}"')
         col2.text_input("✏️ Change Project Name", key="project_name_input")
-        col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name())
+        col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name)
 
-    st.sidebar.write("""
     # Add the page navigation
     st.sidebar.subheader("Pages")
 
@@ -80,19 +77,13 @@ def main():
             module = importlib.import_module(f'pages.{module_name}')
             module.execute()
 
+    st.write("""
     --------------
-    ### Credits:
-    #### Sigma Dimensions (TM)
-
-    Follow us [on Youtube](https://www.youtube.com/channel/UC9bPwuJZUD6ooKqzwdq9M9Q?sub_confirmation=1)
-
     --------------
-    License: MIT
-
+   
     """)
     st.write("")
     st.sidebar.write("")
-
 
 if __name__ == "__main__":
     session = st.session_state
